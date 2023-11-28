08bbfcd5887143acfb25a37e66c9decab8127fb4
Magali Courté <courte.magali@gmail.com>
2023-11-28 16:55:29 +0100

    Maj du main

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-"""Entry point."""
+from controllers import main_controller
 
+# Programme principal
+if __name__ == '__main__':
+    
+    try:
+        main_controller = main_controller.MainController()
+        main_controller.run()
+    except KeyboardInterrupt:
+        print("Arret du programme")
