272d53adc053ccdd0787fe1a42ded79ac00b297b
Magali Courté <courte.magali@gmail.com>
2023-12-06 16:41:55 +0100

    Mise à jour du main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from controllers import main_controller
 
 # Programme principal
 if __name__ == '__main__':
-    
+
     try:
         main_controller = main_controller.MainController()
         main_controller.run()
